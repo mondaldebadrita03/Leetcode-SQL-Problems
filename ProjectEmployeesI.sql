2165e5a8d9a1044e7a8e20112de15aa5f0a249a5
Debadrita Mondal <111679707+mondaldebadrita03@users.noreply.github.com>
2026-08-08 01:19:01 +0530

    Update ProjectEmployeesI.sql

--- ProjectEmployeesI.sql
+++ ProjectEmployeesI.sql
@@ -2,5 +2,5 @@ SELECT p.project_id, ROUND(AVG(e.experience_years),2)
 AS average_years
 FROM Project p
 JOIN Employee e
-ON e.employee_id = p.employee_id
+ON p.employee_id = e.employee_id
 GROUP BY p.project_id;
